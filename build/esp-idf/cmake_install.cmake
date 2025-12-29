d0a01d8a8c458f1cb0dfba2f870cbd56a3493851
Ashfaque Khan <42895491+ashfaquekhan@users.noreply.github.com>
2025-12-29 18:03:46 +0530

    psdf generated and saved

--- build/esp-idf/cmake_install.cmake
+++ build/esp-idf/cmake_install.cmake
@@ -554,7 +554,12 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/ashfa/NirogScanLite/build/esp-idf/espressif__led_strip/cmake_install.cmake")
+  include("C:/Users/ashfa/NirogScanLite/build/esp-idf/espressif__tinyusb/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("C:/Users/ashfa/NirogScanLite/build/esp-idf/espressif__esp_tinyusb/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
